bbce2dfed079c9a2e0dece52fb7ab5b29e36f0f9
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-06 18:33:37 +0530

    Add regegx for Lambda

---
+++ LambdaExpressionRegex/LambdaExpressionRegex/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LambdaExpressionRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------*Registration form*--------");
+            Console.WriteLine("Enter choice between 1-8");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("First Name(First letter  capital)");
+                    string FistName = Console.ReadLine();
+                    if (!isValid.checkFirstName(FistName))
+                    {
+                        Console.WriteLine("enter valid name");
+
+                    }
+                    break;
+
+
+                case 2:
+                    Console.WriteLine("First Name(First letter capital)");
+                    string LastName = Console.ReadLine();
+                    if (!isValid.checkLastName(LastName))
+                    {
+                        Console.WriteLine(" invalid last name");
+
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter the eMail");
+                    string MailId = Console.ReadLine();
+                    if (!isValid.checkEmail(MailId))
+                    {
+                        Console.WriteLine(" invalid");
+
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("Enter the Mobil number");
+                    string MobileNumber = Console.ReadLine();
+                    if (!isValid.checkMobile(MobileNumber))
+                    {
+                        Console.WriteLine("Mobile number is invalid");
+
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("Enter the Password(enter min 8 charector)");
+                    string Password = Console.ReadLine();
+                    if (!isValid.checkPassword(Password))
+                    {
+                        Console.WriteLine(" invalid password");
+
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Enter the Password");
+                    string Password1 = Console.ReadLine();
+                    if (!isValid.checkPassword1(Password1))
+                    {
+                        Console.WriteLine(" invalid password");
+
+                    }
+                    break;
+                case 7:
+                    Console.WriteLine("Enter the Password(");
+                    string Password2 = Console.ReadLine();
+                    if (!isValid.checkPassword2(Password2))
+                    {
+                        Console.WriteLine(" invalid password");
+
+                    }
+                    break;
+                case 8:
+                    Console.WriteLine("Enter the Password");
+                    string Password3 = Console.ReadLine();
+                    if (!isValid.checkPassword3(Password3))
+                    {
+                        Console.WriteLine(" invalid password");
+
+                    }
+                    break;
+            }
+    }
+}
---
+++ LambdaExpressionRegex/LambdaExpressionRegex/isValid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LambdaExpressionRegex
+{
+    class isValid
+    {
+        public static bool checkFirstName(string s)
+        {
+            string regex = "^([A-Z][a-z]{2}[a-z]*)$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkLastName(string s)
+        {
+            string regex = "^([A-Z][a-z]{2}[a-z]*)$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkEmail(string s)
+        {
+            string regex = "^([A-Za-z]+)([.])([A-Za-z]*)([@])([A-Za-z]+)([.])([A-Za-z]+)([.]*)([A-Za-z]*)$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkMobile(string s)
+        {
+            string regex = "^[+][9][1][7-9][0-9]{9}$";//[+][9][1][" "][7-9][0-9]{9}$-->with space
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkPassword(string s)
+        {
+            string regex = "^([0-9a-zA-Z]{8})([a-zA-Z0-9]+)$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkPassword1(string s)
+        {
+            string regex = "(?=.*[A-Z])^[A-Za-z0-9]{8,}$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkPassword2(string s)
+        {
+            string regex = "(?=.*[A-Z])(?=.*[0-9])^[A-Za-z0-9]{8,}$";
+            return Regex.IsMatch(s, regex);
+        }
+        public static bool checkPassword3(string s)
+        {
+            string regex = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@?$])^[A-Za-z0-9!@?$]{8,}$";
+            return Regex.IsMatch(s, regex);
+        }
+    }
+}
